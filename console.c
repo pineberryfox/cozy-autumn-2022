90cdebfc205730057436ee23db8ab1982922003d
Dakotah Lambert <49007668+vvulpes0@users.noreply.github.com>
2022-09-17 06:41:09 -0400

    add basic controller support

--- console.c
+++ console.c
@@ -12,7 +12,96 @@ static double const _dt = 1.0 / 60.0;
 static double _times[16];
 static int _time_i;
 
+static SDL_GameController* _pads[16];
+static int _pad_ids[16];
+static int _num_pads;
+
 void (*cn_quit_hook)(void) = NULL;
+
+static void
+add_controller(int id)
+{
+	SDL_GameController *pad;
+	if (SDL_IsGameController(id))
+	{
+		if (_num_pads >= sizeof(_pads) / sizeof(_pads[0]))
+		{
+			return;
+		}
+		pad = SDL_GameControllerOpen(id);
+		if (pad)
+		{
+			_pads[_num_pads] = pad;
+			_pad_ids[_num_pads] = id;
+			++_num_pads;
+		}
+	}
+}
+static void
+remove_controller(int id)
+{
+	int i;
+	int x = 0;
+	for (i = 0; i < sizeof(_pads) / sizeof(_pads[0]); ++i)
+	{
+		if (_pad_ids[i] == id)
+		{
+			SDL_GameControllerClose(_pads[i]);
+			x = 1;
+			break;
+		}
+	}
+	if (!x) { return; }
+	for (; i < sizeof(_pads) / sizeof(_pads[0]) - 1; ++i)
+	{
+		_pads[i] = _pads[i + 1];
+		_pad_ids[i] = _pad_ids[i + 1];
+	}
+	--_num_pads;
+}
+static int
+button_map(SDL_GameControllerButton b)
+{
+	switch (b)
+	{
+	case SDL_CONTROLLER_BUTTON_A:
+		return CN_BTN_A;
+	case SDL_CONTROLLER_BUTTON_B:
+		return CN_BTN_A;
+	case SDL_CONTROLLER_BUTTON_X:
+		return CN_BTN_B;
+	case SDL_CONTROLLER_BUTTON_Y:
+		return CN_BTN_B;
+	case SDL_CONTROLLER_BUTTON_START:
+		return CN_BTN_START;
+	case SDL_CONTROLLER_BUTTON_DPAD_LEFT:
+		return CN_BTN_LEFT;
+	case SDL_CONTROLLER_BUTTON_DPAD_RIGHT:
+		return CN_BTN_RIGHT;
+	case SDL_CONTROLLER_BUTTON_DPAD_UP:
+		return CN_BTN_UP;
+	case SDL_CONTROLLER_BUTTON_DPAD_DOWN:
+		return CN_BTN_DOWN;
+	default:
+		break;
+	}
+	return 0;
+}
+
+static int
+axis_map(SDL_GameControllerAxis a, int pos)
+{
+	switch (a)
+	{
+	case SDL_CONTROLLER_AXIS_LEFTY:
+		return pos<0 ? CN_BTN_UP : CN_BTN_DOWN;
+	case SDL_CONTROLLER_AXIS_LEFTX:
+		return pos<0 ? CN_BTN_LEFT : CN_BTN_RIGHT;
+	default:
+		break;
+	}
+	return 0;
+}
 
 static void
 _quit(int status)
@@ -33,7 +122,8 @@ cn_init(char *title)
 	{
 		_times[i] = 0;
 	}
-	SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO);
+	SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO
+	         | SDL_INIT_GAMECONTROLLER);
 	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, 0);
 #ifdef __APPLE__
 	SDL_SetHint(SDL_HINT_VIDEO_MAC_FULLSCREEN_SPACES, 0);
@@ -46,6 +136,12 @@ cn_init(char *title)
 	fb_init(&cn_screen, 240, 160, title);
 	_prev_time = SDL_GetPerformanceCounter();
 	_now = _prev_time;
+	for (i = 0; i < sizeof(_pads)/sizeof(_pads[0]); ++i)
+	{
+		/* try to add all the controllers on startup! */
+		add_controller(i);
+	}
+	printf("%d\n", _num_pads);
 	cn_update();
 }
 
@@ -53,7 +149,7 @@ void
 cn_update(void)
 {
 	SDL_Event e;
-	
+
 	double _frame_time;
 	_now = SDL_GetPerformanceCounter();
 	_frame_time = (double)(_now - _prev_time)
@@ -69,6 +165,27 @@ cn_update(void)
 		case SDL_QUIT:
 			_quit(0);
 			break;
+		case SDL_CONTROLLERDEVICEADDED:
+			add_controller(e.cdevice.which);
+			break;
+		case SDL_CONTROLLERDEVICEREMOVED:
+			remove_controller(e.cdevice.which);
+			break;
+		case SDL_CONTROLLERBUTTONDOWN:
+			cn_buttons |= button_map(e.cbutton.button);
+			break;
+		case SDL_CONTROLLERBUTTONUP:
+			cn_buttons &= ~button_map(e.cbutton.button);
+			break;
+		case SDL_CONTROLLERAXISMOTION:
+			/* dead zone */
+			cn_buttons &= ~axis_map(e.caxis.axis, -30000);
+			cn_buttons &= ~axis_map(e.caxis.axis,  30000);
+			if (500 < e.caxis.value || e.caxis.value < -500)
+			{
+				cn_buttons |= axis_map(e.caxis.axis,
+				                       e.caxis.value);
+			}
 		case SDL_KEYDOWN:
 			switch (e.key.keysym.scancode)
 			{
@@ -134,6 +251,8 @@ cn_update(void)
 			default:
 				break;
 			}
+			break;
+		default:
 			break;
 		}
 	}
